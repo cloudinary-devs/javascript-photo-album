c497e23079c509aca0d715f6a7853cb94767fb39
Tamas Piros <tpiros@tpiros-MacBook-Air.local>
2023-12-21 14:07:00 +0800

    fixed production build issue

--- router/router.js
+++ router/router.js
@@ -9,36 +9,41 @@ class Router {
     const path = window.location.pathname;
     switch (path) {
       case '/':
-        await this.loadComponent('home-page', '../pages/home-page.js');
+        // await this.loadComponent('home-page', '../pages/home-page');
+        await import('../pages/home-page.js');
+        this.layout.loadPage(`<home-page></home-page>`);
         break;
       case '/upload':
-        await this.loadComponent('upload-page', '../pages/upload-page.js');
+        await import('../pages/upload-page.js');
+        this.layout.loadPage(`<upload-page></upload-page>`);
+        // await this.loadComponent('upload-page', '../pages/upload-page');
         this.loadUploadWidgetScript();
         break;
       case '/apiupload':
-        await this.loadComponent(
-          'api-upload-page',
-          '../pages/api-upload-page.js'
-        );
+        await import('../pages/api-upload-page.js');
+        this.layout.loadPage(`<api-upload-page></api-upload-page>`);
+        // await this.loadComponent('api-upload-page', '../pages/api-upload-page');
         break;
       case '/album':
-        await this.loadComponent('album-page', '../pages/album-page.js');
+        await import('../pages/album-page.js');
+        this.layout.loadPage(`<album-page></album-page>`);
+        // await this.loadComponent('album-page', '../pages/album-page');
         break;
       default:
         this.layout.loadPage('<p>Page not found</p>');
     }
   }
 
-  async loadComponent(tagName, modulePath) {
-    if (!customElements.get(tagName)) {
-      try {
-        await import(modulePath);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    this.layout.loadPage(`<${tagName}></${tagName}>`);
-  }
+  // async loadComponent(tagName, modulePath) {
+  //   if (!customElements.get(tagName)) {
+  //     try {
+  //       await import(`${modulePath}.js`);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
+  //   this.layout.loadPage(`<${tagName}></${tagName}>}`);
+  // }
 
   loadUploadWidgetScript() {
     const uploadPage = document.querySelector('upload-page');
